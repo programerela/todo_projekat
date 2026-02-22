14b263e346d840d5220b68edf3b26c3417b97987
Ela Curic <curicela@gmail.com>
2026-02-22 10:38:22 +0100

    router

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,17 +1,47 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+
 import Homepage from "./pages/Home.jsx";
 import Navbar from "./component/shared/Navbar.jsx";
 import MyTasks from "./pages/MyTasks.jsx";
-import './App.css'
+import Footer from "./component/shared/Footer.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
 
-function App() {
+function AppContent() {
+  const location = useLocation();
+
+  const hideNavbar =
+    location.pathname === "/login" || location.pathname === "/register";
+
+  const hideFooter =
+    location.pathname === "/login" || location.pathname === "/register";
 
   return (
     <div>
-      <Navbar />
-      <Homepage />
-      <MyTasks />
+      {!hideNavbar && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/my-tasks" element={<MyTasks />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Homepage />} />
+      </Routes>
+      {!hideFooter && <Footer />}
     </div>
-  )
+  );
 }
 
-export default App
+function App() {
+  return (
+    <Router>
+      <AppContent />
+    </Router>
+  );
+}
+
+export default App;
--- todo/src/component/shared/Footer.jsx
+++ todo/src/component/shared/Footer.jsx
@@ -1,1 +1,7 @@
-export default function Footer() {}
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <p>&copy; 2026 ToDo App. All rights reserved.</p>
+        </footer>
+    );
+}
